15a2558e1a7fc3423cfb1cda47b573491baefe0b
Hsutra <96309579+Hsutra@users.noreply.github.com>
2023-03-24 15:13:34 +0300

    Update syntax-parser.py

--- syntax-parser.py
+++ syntax-parser.py
@@ -1,8 +1,14 @@
+def is_float(text):
+    try:
+        float(text)
+        return True
+    except ValueError:
+        return False
+
 def ComandList(text):
     Operations = ['+', '-', '*', '/', '%', '**', '<', '>', '==', '!=', '<=', '>=', '=', '++', '--']
-    Words = ['let', 'var', 'alert', 'if', 'else if', 'else', 'while', 'do', 'for', 'return']
-    Separators = [' ', 's', ';', '{', '}', '[', ']', '(', ')']
-    Digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    Words = ['let', 'var', 'alert', 'if', 'else if', 'else', 'while', 'do', 'for']
+    Separators = ['\n', ';', '{', '}', '[', ']', '(', ')']
     Dict = {}
     a = 1
     b = 1
@@ -18,20 +24,27 @@ def ComandList(text):
     for i in Separators:
         Dict[i] = "R" + str(c)
         c += 1
-    for i in Digits:
-        Dict[i] = "D" + str(d)
-        d += 1
+
+    for i in text:
+        if((i.isdigit() or is_float(i)) and not(i in Dict)):
+            Dict[i] = "D" + str(d)
+            d += 1
+
     for i in text:
         if not(i in Dict):
             Dict[i] = "I" + str(e)
             e += 1
     Endlist = []
+
     for i in text:
         if(i in Dict):
             Endlist.append(Dict[i])
     return Endlist
 
-text = "if ( a > b ) a ++ 1 else b ++"
-text = text.split()
+f = open('file', 'r')
+txt = ""
+for i in f:
+    txt += i
+text = txt.split()
 print(ComandList(text))
 
